cdae41852b152af4c238db52b7a280e50643a603
MariaVoreakou <voreakou7@gmail.com>
2017-10-02 20:48:29 +0300

    Add files via upload

---
+++ src/crosswordexpress/CrosswordExpress.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crosswordexpress;
+
+
+/**
+ *
+ * @author Maria
+ */
+public class CrosswordExpress {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+
+        
+
+        new GUI();
+
+       
+
+        
+        //JScrollPane scrollPane = new JScrollPane(table);
+        //table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        //frame.add(scrollPane, BorderLayout.CENTER);
+      
+        
+
+    }
+}
